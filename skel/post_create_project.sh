ca3bd2a6b4a8f3efc4108e2bf2352e2a9d80813c
Thomas Rieschl <thomas@trinet.at>
2015-01-12 19:47:36 +0100

    cleanup post create script; delete script after execution

--- skel/post_create_project.sh
+++ skel/post_create_project.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 
-pwd
-
 cp ./environment.php.dist ./environment.php
-#cp ./application/config/config.php.dist ./application/config/config.php
-#cp ./application/config/database.php.dist ./application/config/database.php
-
 cp -a ./vendor/codeigniter/framework/application .
+rm -rf ./skel
