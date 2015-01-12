a124188d2c0868f42a9194014329877a5c98763e
Thomas Rieschl <thomas@trinet.at>
2015-01-12 19:31:31 +0100

    add post create project script

---
+++ skel/post_create_project.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+cp ../environment.php.dist ../environment.php
+cp ../application/config/config.php.dist ../application/config/config.php
+cp ../application/config/database.php.dist ../application/config/database.php
