d72d348588afe6a3b561f40068d05c18787357f2
Thomas Rieschl <thomas@trinet.at>
2015-01-12 19:43:30 +0100

    remove application folder; instead, copy it after creation of project directly from the vendor directory

--- skel/post_create_project.sh
+++ skel/post_create_project.sh
@@ -3,5 +3,7 @@
 pwd
 
 cp ./environment.php.dist ./environment.php
-cp ./application/config/config.php.dist ./application/config/config.php
-cp ./application/config/database.php.dist ./application/config/database.php
+#cp ./application/config/config.php.dist ./application/config/config.php
+#cp ./application/config/database.php.dist ./application/config/database.php
+
+cp -a ./vendor/codeigniter/framework/application .
